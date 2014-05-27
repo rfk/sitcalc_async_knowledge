02f5cf1bea4bfdb746c4e510b5f12c98faf13371
Ryan Kelly <ryan@rfk.id.au>
2014-05-27 20:08:59 +1000

    Importing code into stand-alone repository.

---
+++ main.pl
@@ -0,0 +1,12 @@
+
+:- set_prolog_flag(double_quotes, atom).
+:- set_prolog_stack(local, limit(2*10**9)).
+
+:- use_module(library(plunit)).
+
+:- [utils].
+:- [prover].
+:- [fluent].
+:- [sitcalc].
+:- [domain_party].
+
